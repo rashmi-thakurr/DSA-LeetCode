aed6c93728cb7c2f4acb9e222b8d96a175ad4fee
Rashmi Thakur <66085091+rashmi-thakurr@users.noreply.github.com>
2022-04-17 01:26:30 +0530

    Added solution - LeetHub

---
+++ Reverse Bits - GFG/reverse-bits.cpp
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    unsigned int reverseBits(unsigned int n)
+    {
+        //code here
+        int ans = 0;
+        
+        while(n){
+            ans <<= 1;
+            
+            if(n&1){
+                ans = ans ^ 1;
+            }
+            
+            n >>= 1;
+            
+        }
+        
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        
+        Solution ob;
+        cout<< ob.reverseBits(N) <<"\n";
+        
+    }
+    return 0;
+}
+  // } Driver Code Ends
