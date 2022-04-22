c4912181e05eba926f6d7dfbe9db5d7f7961a348
Rashmi Thakur <66085091+rashmi-thakurr@users.noreply.github.com>
2022-04-22 13:08:29 +0530

    Added solution - LeetHub

--- Bit Difference - GFG/bit-difference.cpp
+++ Bit Difference - GFG/bit-difference.cpp
@@ -12,37 +12,37 @@ public:
     int countBits(int N, long long int A[])
     {
         // code here
-        // int ans=0;
-        // long long int m = 1000000007;
+        long long int ans=0;
+        long long int m = 1000000007;
         
-        // for(int i=0;i<32;i++){
-        //     long long int count=0;
-        //     for(int j=0;j<N;j++){
-        //         if((A[j] & (1<<i))){
-        //             count++;
-        //         }
-        //     }
-            
-        //     ans += ((count%m)*((N-count)%m)*2)%m;
-        // }
-        
-        // return ans%m;
-        
-         const int p = 1000000007;
-        long long int ans = 0;
-        for(int j=0; j<32; j++)
-        {
-            long long int n = 0, m=0;
-            for(int i=0; i<N; i++)
-            {
-                if(A[i]&(1<<j))
-                    n++;
-                else
-                    m++;
+        for(int i=0;i<32;i++){
+            long long int count=0;
+            for(int j=0;j<N;j++){
+                if((A[j] & (1<<i))){
+                    count++;
+                }
             }
-            ans = (ans + 2*n*m)%p;
+            
+            ans += ((count%m)*((N-count)%m)*2)%m;
         }
-        return ans;
+        
+        return ans%m;
+        
+        //  const int p = 1000000007;
+        // long long int ans = 0;
+        // for(int j=0; j<32; j++)
+        // {
+        //     long long int n = 0, m=0;
+        //     for(int i=0; i<N; i++)
+        //     {
+        //         if(A[i]&(1<<j))
+        //             n++;
+        //         else
+        //             m++;
+        //     }
+        //     ans = (ans + 2*n*m)%p;
+        // }
+        // return ans;
     }
 };
 
