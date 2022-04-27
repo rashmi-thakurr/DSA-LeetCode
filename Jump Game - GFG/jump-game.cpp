a772516ab4605e5d509c123ee5bbb0bfbb1585d9
Rashmi Thakur <66085091+rashmi-thakurr@users.noreply.github.com>
2022-04-27 14:15:39 +0530

    Added solution - LeetHub

---
+++ Jump Game - GFG/jump-game.cpp
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int canReach(int A[], int N) {
+        // code here
+        int lastInd=N-1;
+        for(int i=N-1;i>=0;i--){
+            if(A[i]+i >= lastInd){
+                lastInd = i;
+            }
+        }
+        
+        return (lastInd==0) ? 1: 0;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        
+        int A[N];
+        
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.canReach(A,N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
