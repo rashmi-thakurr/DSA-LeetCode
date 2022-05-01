717f8d99203403e005f9f2bafe17bbbd1aaa90b4
Rashmi Thakur <66085091+rashmi-thakurr@users.noreply.github.com>
2022-05-02 02:41:57 +0530

    Added solution - LeetHub

--- Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
+++ Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
@@ -9,46 +9,46 @@ using namespace std;
 
 class Solution{
 public:
-    int maximumPath(int n, vector<vector<int>> arr)
+    int maximumPath(int N, vector<vector<int>> Matrix)
     {
         // code here
-    //     int dp[N][N];
+        int dp[N][N];
         
-    //     dp[0][0] = Matrix[0][0];
+        dp[0][0] = Matrix[0][0];
         
-    //     int ans = Matrix[0][0];
+        int ans = Matrix[0][0];
         
-    //     for(int i=0;i<N;i++){
-    //         for(int j=0;j<N;j++){
-    //             if(i==0) dp[i][j]=Matrix[i][j];
-    //         }
-    //     }
-        
-    //     for(int i=1;i<N;i++){
-    //         for(int j=0;j<N;j++){
-    //             int mx = Matrix[i-1][j];
-    //             if(j!=0) mx = max(mx,Matrix[i-1][j-1]);
-    //             if(j!=N-1) mx = max(mx,Matrix[i-1][j+1]);
-    //             dp[i][j] = Matrix[i][j] + mx;
-    //             if(i==N-1) ans = max(ans,dp[i][j]);
-    //         }
-            
-    //     }
-        
-    //   return ans;
-    
-    int result = arr[0][0];
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int x = (j == 0) ? 0 : arr[i - 1][j - 1];
-                int y = arr[i - 1][j];
-                int z = (j == n - 1) ? 0 : arr[i - 1][j + 1];
-                arr[i][j] += max({ x, y, z });
-                if (i == n - 1)
-                    result = max(result, arr[i][j]);
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(i==0) dp[i][j]=Matrix[i][j];
             }
         }
-        return result;
+        
+        for(int i=1;i<N;i++){
+            for(int j=0;j<N;j++){
+                int mx = dp[i-1][j];
+                if(j!=0) mx = max(mx,dp[i-1][j-1]);
+                if(j!=N-1) mx = max(mx,dp[i-1][j+1]);
+                dp[i][j] = Matrix[i][j] + mx;
+                if(i==N-1) ans = max(ans,dp[i][j]);
+            }
+            
+        }
+        
+      return ans;
+    
+    // int result = arr[0][0];
+    //     for (int i = 1; i < n; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             int x = (j == 0) ? 0 : arr[i - 1][j - 1];
+    //             int y = arr[i - 1][j];
+    //             int z = (j == n - 1) ? 0 : arr[i - 1][j + 1];
+    //             arr[i][j] += max({ x, y, z });
+    //             if (i == n - 1)
+    //                 result = max(result, arr[i][j]);
+    //         }
+    //     }
+    //     return result;
     }
 };
 
