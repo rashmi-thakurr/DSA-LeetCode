d30f6933d88b871a2f2498fa0971c8fceee1205c
Rashmi Thakur <66085091+rashmi-thakurr@users.noreply.github.com>
2022-05-02 01:55:27 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -9,25 +9,21 @@ using namespace std;
 
 class Solution{   
 public:
-    bool isSubsetSum(int N, int arr[], int sum){
+    bool isSubsetSum(vector<int>arr, int sum){
         // code here 
-        bool dp[N+1][sum+1];
+        int n=arr.size();
+        int dp[n+1][sum+1];
         
-        for(int i=0;i<N+1;i++){
+        for(int i=0;i<n+1;i++){
             for(int j=0;j<sum+1;j++){
-                if(i==0){
-                    dp[i][j] = false;
-                }
-                   
-                if(j==0){
-                    dp[i][j] = true;
-                }
+                if(i==0) dp[i][j]=0;
+                if(j==0) dp[i][j]=1;
             }
         }
         
-        for(int i=1;i<N+1;i++){
+        for(int i=1;i<n+1;i++){
             for(int j=1;j<sum+1;j++){
-                if(arr[i-1]<sum){
+                if(arr[i-1]<=j){
                     dp[i][j] = dp[i-1][j-arr[i-1]] or dp[i-1][j];
                 }
                 else{
@@ -36,7 +32,9 @@ public:
             }
         }
         
-        return dp[N][sum];
+        return dp[n][sum];
+        
+        
     }
 };
 
@@ -49,14 +47,14 @@ int main()
     {
         int N, sum;
         cin >> N;
-        int arr[N];
+        vector<int> arr(N);
         for(int i = 0; i < N; i++){
             cin >> arr[i];
         }
         cin >> sum;
         
         Solution ob;
-        cout << ob.isSubsetSum(N, arr, sum) << endl;
+        cout << ob.isSubsetSum(arr, sum) << endl;
     }
     return 0; 
 } 
