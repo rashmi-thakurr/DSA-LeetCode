30f8d636f94630f52abe677d3afb0ca90000f3ed
Rashmi Thakur <66085091+rashmi-thakurr@users.noreply.github.com>
2022-05-02 12:03:07 +0530

    Added solution - LeetHub

--- Form a palindrome - GFG/form-a-palindrome.cpp
+++ Form a palindrome - GFG/form-a-palindrome.cpp
@@ -1,60 +1,59 @@
 // { Driver Code Starts
 //Initial template for C++
 
-#include <bits/stdc++.h>
-using namespace std;
+#include<bits/stdc++.h> 
+using namespace std; 
 
  // } Driver Code Ends
 //User function template for C++
 
-class Solution{
-  public:
-    int countMin(string str){
-    //complete the function here
-    int n = str.size();
-    
-    int dp[n+1][n+1];
-    
-    string str2 = str;
-    reverse(str2.begin(),str2.end());
-    
-    
-    for(int i=0;i<n+1;i++){
-        for(int j=0;j<n+1;j++){
-            if(i==0 or j==0){
-                dp[i][j] = 0;
+class Solution{   
+public:
+    int findMinInsertions(string S){
+        // code here
+        string S1 = S;
+        reverse(S1.begin(),S1.end());
+        
+        int n=S.length();
+        int dp[n+1][n+1];
+        
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<n+1;j++){
+                if(i==0 or j==0){
+                    dp[i][j]=0;
+                }
             }
         }
-    }
-    
-    for(int i=1;i<n+1;i++){
-        for(int j=1;j<n+1;j++){
-            if(str[i-1]==str2[j-1]){
-                dp[i][j] = 1+dp[i-1][j-1];
-            }
-            else{
-                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(S[i-1]==S1[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
-    }
-    
-    
-    return str.size()-dp[n][n];
-    
+        
+        return n-dp[n][n];
+        
     }
 };
 
 // { Driver Code Starts.
+
+
+
 int main(){
     int t;
-    cin >> t;
+    cin>>t;
     while(t--){
-        string str;
-        cin >> str;
+        string S;
+        cin>>S;
         Solution ob;
-        cout << ob.countMin(str) << endl;
+        cout<<ob.findMinInsertions(S)<<endl;
     }
-return 0;
+    return 0;
 }
-
   // } Driver Code Ends
