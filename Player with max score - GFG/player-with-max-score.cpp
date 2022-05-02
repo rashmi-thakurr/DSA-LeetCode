cfe2e5d8c3b16ebe1448ea8b116ff5642eec56bc
Rashmi Thakur <66085091+rashmi-thakurr@users.noreply.github.com>
2022-05-02 11:07:41 +0530

    Added solution - LeetHub

---
+++ Player with max score - GFG/player-with-max-score.cpp
@@ -0,0 +1,38 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    bool is1winner(int N,int arr[]) {
+        
+        // code here
+        return N%2 == 0 ? true : false;
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, X;
+        cin >> N;
+        int arr[N];
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        cout << ob.is1winner(N, arr) << endl;
+    }
+    return 0; 
+} 
+  // } Driver Code Ends
