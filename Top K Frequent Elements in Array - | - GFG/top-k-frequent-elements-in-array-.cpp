c1f24c7183d690443eec97cd45f628afba823047
Rashmi Thakur <66085091+rashmi-thakurr@users.noreply.github.com>
2022-05-12 23:35:51 +0530

    Added solution - LeetHub

--- Top K Frequent Elements in Array - | - GFG/top-k-frequent-elements-in-array-.cpp
+++ Top K Frequent Elements in Array - | - GFG/top-k-frequent-elements-in-array-.cpp
@@ -8,23 +8,23 @@ class Solution {
     vector<int> topK(vector<int>& nums, int k) {
         // Code here
         unordered_map<int,int> mp;
-        vector<pair<int,int>> v;
+       priority_queue<pair<int,int>> minH;
         vector<int> ans;
         
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
         }
         
-        for(auto it:mp){
-            v.push_back({it.second,it.first});
-        }
-        
-        sort(v.begin(),v.end(),greater<pair<int,int>>());
-        
-        for(int i=0;i<k;i++){
-            ans.push_back(v[i].second);
-        }
-        
+       for(auto it=mp.begin();it!=mp.end();it++){
+           minH.push({it->second,it->first});
+       }
+       
+       while(k--){
+           ans.push_back(minH.top().second);
+           minH.pop();
+       }
+       
+       
         return ans;
     }
 };
