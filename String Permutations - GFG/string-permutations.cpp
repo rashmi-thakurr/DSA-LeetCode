6d587dafefe6cb25299d64e3fb82ed6f1f83e5d5
Rashmi Thakur <66085091+rashmi-thakurr@users.noreply.github.com>
2022-05-20 16:05:45 +0530

    Added solution - LeetHub

--- String Permutations - GFG/string-permutations.cpp
+++ String Permutations - GFG/string-permutations.cpp
@@ -7,20 +7,33 @@ class Solution{
     public:
     //Complete this function
     
-    void perm(string S, vector<string> &ans, string &a, vector<int> &vis){
-        if(a.length() == S.length()){
-            ans.push_back(a);
+    // void perm(string S, vector<string> &ans, string &a, vector<int> &vis){
+    //     if(a.length() == S.length()){
+    //         ans.push_back(a);
+    //         return;
+    //     }
+        
+    //     for(int j=0;j<S.length();j++){
+    //       if(vis[j]==0){
+    //         a.push_back(S[j]);
+    //         vis[j]=1;
+    //         perm(S,ans,a,vis);
+    //         vis[j]=0;
+    //         a.pop_back();
+    //       }
+    //     }
+        
+    // }
+    void perm2(string S, vector<string> &ans, int ind){
+        if(ind == S.length()-1){
+            ans.push_back(S);
             return;
         }
         
-        for(int j=0;j<S.length();j++){
-           if(vis[j]==0){
-            a.push_back(S[j]);
-            vis[j]=1;
-            perm(S,ans,a,vis);
-            vis[j]=0;
-            a.pop_back();
-           }
+        for(int j=ind;j<S.length();j++){
+            swap(S[ind],S[j]);
+            perm2(S,ans,ind+1);
+            swap(S[ind],S[j]);
         }
         
     }
@@ -29,12 +42,12 @@ class Solution{
     {
         //Your code here
         vector<string> ans;
-        string a ="";
-        vector<int> vis;
-        for(auto i:S){
-            vis.push_back(0);
-        }
-        perm(S,ans,a,vis);
+        // string a ="";
+        // vector<int> vis;
+        // for(auto i:S){
+        //     vis.push_back(0);
+        // }
+        perm2(S,ans,0);
         
         sort(ans.begin(),ans.end());
         return ans;
