e737f566c49107276c951ee7b61330c6f5bb7a04
Rashmi Thakur <66085091+rashmi-thakurr@users.noreply.github.com>
2022-06-14 18:54:28 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -11,54 +11,51 @@ using namespace std;
 class Solution{
     public:
     void solve(vector<vector<int>> &m, int n, int i, int j, string str, vector<string> &ans){
-        if(i==n-1 and j==n-1){
-            ans.push_back(str);
-            return;
-        }
-        
-        if(i>0 and m[i-1][j]==1){
-            m[i][j] = 0;
-            str.push_back('U');
-            solve(m,n,i-1,j,str,ans);
-            str.pop_back();
-            m[i][j] = 1;
-        }
-        
-         if(j>0 and m[i][j-1]==1){
-            m[i][j] = 0;
-            str.push_back('L');
-            solve(m,n,i,j-1,str,ans);
-            str.pop_back();
-            m[i][j] = 1;
-        }
-        
-         if(i<n-1 and m[i+1][j]==1){
-            m[i][j] = 0;
-            str.push_back('D');
-            solve(m,n,i+1,j,str,ans);
-            str.pop_back();
-            m[i][j] = 1;
-        }
-        
-         if(j<n-1 and m[i][j+1]==1){
-            m[i][j] = 0;
-            str.push_back('R');
-            solve(m,n,i,j+1,str,ans);
-            str.pop_back();
-            m[i][j] = 1;
-        }
+      if(i==n-1 and j==n-1){
+          ans.push_back(str);
+          return;
+      }
+      
+      if(i>0 and m[i-1][j]==1){
+          m[i][j] = 0;
+          str.push_back('U');
+          solve(m,n,i-1,j,str,ans);
+          str.pop_back();
+          m[i][j]=1;
+      }
+      
+      if(i<n-1 and m[i+1][j]==1){
+          m[i][j]=0;
+          str.push_back('D');
+          solve(m,n,i+1,j,str,ans);
+          str.pop_back();
+          m[i][j]=1;
+      }
+      if(j>0 and m[i][j-1]==1){
+          m[i][j]=0;
+          str.push_back('L');
+          solve(m,n,i,j-1,str,ans);
+          str.pop_back();
+          m[i][j]=1;
+      }
+      if(j<n-1 and m[i][j+1]==1){
+          m[i][j]=0;
+          str.push_back('R');
+          solve(m,n,i,j+1,str,ans);
+          str.pop_back();
+          m[i][j]=1;
+      }
     }
     vector<string> findPath(vector<vector<int>> &m, int n) {
         // Your code goes here
-        
-        string str;
-        vector<string> ans;
-        if(m[0][0]==0){
-            ans.push_back("-1");
-            return ans;
-        };
-        solve(m,n,0,0,str,ans);
-        return ans;
+      vector<string> ans;
+      string str;
+      if(m[0][0] == 0){
+          ans.push_back("-1");
+          return ans;
+    }
+      solve(m,n,0,0,str,ans);
+      return ans;
     }
 };
 
