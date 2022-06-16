39ba4fe1d0db831c67e21d72a4432bb2e8ffb607
Rashmi Thakur <66085091+rashmi-thakurr@users.noreply.github.com>
2022-06-16 16:06:17 +0530

    Time: 337 ms (95.27%), Space: 117.1 MB (66.05%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -2,33 +2,33 @@ class MedianFinder {
 public:
     
     //We meed two pqs. minHeap would be required to store the second half of the data and maxGeap would store the first half of the data. Median would be the average of two elements at the top of the two heaps in case if the size of two heaps are equal else the median would be the top element of the heap with greater size with the size diff of the two heaps not more than 1.
-    priority_queue<int> mnH; //maxheap
-    priority_queue<int,vector<int>,greater<int>> mxH; //mnheap
+    priority_queue<int> mxH; //maxheap
+    priority_queue<int,vector<int>,greater<int>> mnH; //mnheap
     
     MedianFinder() {
-      mnH.push(1);
-        mnH.pop();
+      mxH.push(1);
+        mxH.pop();
     }
     
     void addNum(int num) {
-        if(mnH.top()>num || mnH.size()==0) mnH.push(num);
-        else mxH.push(num);
+        if(mxH.top()>num || mxH.size()==0) mxH.push(num);
+        else mnH.push(num);
         
         //Balancing the heaps;
-        while(mnH.size() > mxH.size()+1){
-            mxH.push(mnH.top());
-            mnH.pop();
-        }
-        
         while(mxH.size() > mnH.size()+1){
             mnH.push(mxH.top());
             mxH.pop();
+        }
+        
+        while(mnH.size() > mxH.size()+1){
+            mxH.push(mnH.top());
+            mnH.pop();
         }
     }
     
     double findMedian() {
         if(mnH.size()==mxH.size()){
-            if(mnH.empty()) return 0;
+            if(mxH.empty()) return 0;
             return (mnH.top() + mxH.top())/2.0;
         }
         else if(mnH.size() > mxH.size()){
