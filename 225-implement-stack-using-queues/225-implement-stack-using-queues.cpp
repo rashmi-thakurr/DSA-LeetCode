460775f663449e0bc0cb42b7387d4619c5061c36
Rashmi Thakur <66085091+rashmi-thakurr@users.noreply.github.com>
2022-06-17 09:25:31 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (87.01%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -6,7 +6,7 @@ public:
     }
     
     void push(int x) {
-        q.push(x);
+       q.push(x);
         for(int i=0;i<q.size()-1;i++){
             q.push(q.front());
             q.pop();
@@ -14,8 +14,8 @@ public:
     }
     
     int pop() {
-       int x = q.front();
-       q.pop(); 
+      int x = q.front();
+        q.pop();
         return x;
     }
     
@@ -24,12 +24,7 @@ public:
     }
     
     bool empty() {
-        if(q.empty()){
-            return true;
-        }
-        
-        return false;
-        
+        return q.empty();      
     }
 };
 
