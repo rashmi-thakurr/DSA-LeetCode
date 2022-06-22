aae01c9b4c60b63e2fa4c4550e632f456b3361c5
Rashmi Thakur <66085091+rashmi-thakurr@users.noreply.github.com>
2022-06-22 09:41:34 +0530

    Time: 4 ms (71.95%), Space: 12.6 MB (32.10%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -11,19 +11,37 @@
  */
 class Solution {
 public:
-   void solve(TreeNode* root, int level, map<int,vector<int>> &mp){
-       if(!root) return;
-       mp[level].push_back(root->val);
-       if(root->left) solve(root->left, level+1, mp);
-       if(root->right) solve(root->right, level+1, mp);
-   }
+   // void solve(TreeNode* root, int level, map<int,vector<int>> &mp){
+   //     if(!root) return;
+   //     mp[level].push_back(root->val);
+   //     if(root->left) solve(root->left, level+1, mp);
+   //     if(root->right) solve(root->right, level+1, mp);
+   // }
     vector<vector<int>> levelOrder(TreeNode* root) {
       
-       vector<vector<int>> ans;
-        map<int,vector<int>> mp;
-        solve(root,0,mp);
-        for(auto i:mp){
-            ans.push_back(i.second);
+       // vector<vector<int>> ans;
+       //  map<int,vector<int>> mp;
+       //  solve(root,0,mp);
+       //  for(auto i:mp){
+       //      ans.push_back(i.second);
+       //  }
+       //  return ans;
+        vector<vector<int>> ans;
+        if(!root) return {};
+        if(!root->left and !root->right) return {{root->val}};
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int sz = q.size();
+            vector<int> v;
+            for(int i=0;i<sz;i++){
+                TreeNode* p = q.front();
+                q.pop();
+                v.push_back(p->val);
+                if(p->left) q.push(p->left);
+                if(p->right) q.push(p->right);
+            }
+            ans.push_back(v);
         }
         return ans;
     }
